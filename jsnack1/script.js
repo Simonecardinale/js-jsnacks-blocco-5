22c409c6b10ce78b0beaaf24b9c3c75048eb6398
Simone Cardinale <simonecardinale8@gmail.com>
2021-01-18 14:44:04 +0100

    jsnack 1 work complete

---
+++ jsnack1/script.js
@@ -0,0 +1,11 @@
+var palla = {
+    nome: 'palla',
+    peso: 10
+};
+
+console.log(palla);
+
+var utente = parseInt(prompt("Inserisci il nuovo peso della palla"));
+palla.peso = utente
+
+console.log(palla);
